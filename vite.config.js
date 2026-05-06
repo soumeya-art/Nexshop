90dcb3b56153010c9803cc6765b5216d87bff0c2
soumeya-art <soumeyabachir123@gmail.com>
2026-05-06 01:51:17 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,11 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        emptyOutDir: true,
+    },
     server: {
         watch: {
             ignored: ['**/storage/framework/views/**'],
